c438e3a16fb088561e09819f5c24086080b82634
Di Chen <chenditc@umich.edu>
2016-07-24 20:53:53 +0000

    Fix url error. Use an array of cellid as workload

--- pgoworker/fetch/views.py
+++ pgoworker/fetch/views.py
@@ -6,18 +6,17 @@ from django.http import HttpResponse, HttpResponseBadRequest
 
 import pogo_client
 
-def health(request):
+def query(request):
     try:
-        data = json.loads(request.body) 
-        cellid = int(data['cellid'])
+        cell_ids = json.loads(request.body) 
+        cell_ids = [ int(cell_id) for cell_id in cell_ids ]
     except:
         logging.getLogger('worker').info("Fail to parse cellid from {0}".format(data))
         return HttpResponseBadRequest("Fail to parse cellid from {0}".format(data))
 
-
-    rcode = pogo_client.query_cellid(cellid)
-
-    if rcode != 0:
-        logging.getLogger('worker').info("Failed to query cell id {0}".format(data))
-        return HttpResponseBadRequest("Failed to query cell id, rcode {0}".format(rcode))
+    for cell_id in cell_ids:
+        rcode = pogo_client.query_cellid(cell_id)
+        if rcode != 0:
+            logging.getLogger('worker').info("Failed to query cell id {0}".format(data))
+            return HttpResponseBadRequest("Failed to query cell id, rcode {0}".format(rcode))
     return HttpResponse("OK")
